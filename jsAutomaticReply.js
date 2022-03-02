4489798be58af868c925a21474508379dad8cade
宏 <977106024@qq.com>
2022-03-02 15:24:30 +0800

    dom每次需要重新获取

--- jsAutomaticReply.js
+++ jsAutomaticReply.js
@@ -1,8 +1,10 @@
-const button = document.querySelector('.') // 评论按钮
-const input1 = document.querySelector('.') // 输入框
 
 
 const reply = () => {
+    const button = document.querySelector('.') // 评论按钮
+    const input1 = document.querySelector('.') // 输入框
+    button.disabled = false
+
     // 随机内容
     const sendTextArr = ['[嘻嘻]', '[哈哈]', '5299', '冲啊！', '互动', '[舔屏]', '[doge]', '[二哈]', '周周yyds', '[心]']
     const n = Math.floor(Math.random() * sendTextArr.length + 1) - 1;
@@ -23,11 +25,11 @@ const reply = () => {
 
 reply()
 
-// 十分钟评论一次
+// n分钟评论一次
 setInterval(() => {
     reply()
-    console.log('10分钟到')
-}, 60000 * 10)
+    console.log('10分钟到',new Date())
+}, 60000 * 16)
 
 
 
