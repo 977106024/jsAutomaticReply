9ec0f42f9ec48341b4126740fdd425d8aa6982f7
宏 <977106024@qq.com>
2022-03-02 11:28:41 +0800

    异步问题

--- jsAutomaticReply.js
+++ jsAutomaticReply.js
@@ -3,19 +3,22 @@ const input1 = document.querySelector('.') // 输入框
 
 
 const reply = () => {
-     // 随机内容
-     const sendTextArr = ['[嘻嘻]', '[哈哈]', '5299', '冲啊！', '互动', '[舔屏]', '[doge]', '[二哈]', '周周yyds', '[心]']
-     const n = Math.floor(Math.random() * sendTextArr.length + 1) - 1;
-     const text = sendTextArr[n]
- 
-     // 模拟输入事件 像文本框中输入内容
-     let evt1 = document.createEvent('HTMLEvents');
-     evt1.initEvent('input', true, true);
-     input1.value = text;
-     input1.dispatchEvent(evt1)
- 
-     // 点击评论按钮 提交评论
-     button.click()
+    // 随机内容
+    const sendTextArr = ['[嘻嘻]', '[哈哈]', '5299', '冲啊！', '互动', '[舔屏]', '[doge]', '[二哈]', '周周yyds', '[心]']
+    const n = Math.floor(Math.random() * sendTextArr.length + 1) - 1;
+    const text = sendTextArr[n]
+
+    // 模拟输入事件 像文本框中输入内容
+    let evt1 = document.createEvent('HTMLEvents');
+    evt1.initEvent('input', true, true);
+    input1.value = text;
+    input1.dispatchEvent(evt1)
+
+    // 点击评论按钮 提交评论
+    setTimeout(() => {
+        button.click()
+        console.log('评论！')
+    }, 2000)
 }
 
 reply()
@@ -23,7 +26,9 @@ reply()
 // 十分钟评论一次
 setInterval(() => {
     reply()
+    console.log('10分钟到')
 }, 60000 * 10)
+
 
 
 
