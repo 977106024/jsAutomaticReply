3b735a72eeb7546fd724ef9bda9e688a34a6f672
宏 <977106024@qq.com>
2022-03-02 10:56:22 +0800

    添加随机内容

--- jsAutomaticReply.js
+++ jsAutomaticReply.js
@@ -1,14 +1,30 @@
 const button = document.querySelector('.') // 评论按钮
 const input1 = document.querySelector('.') // 输入框
 
-// 模拟输入事件 像文本框中输入内容
-let evt1 = document.createEvent('HTMLEvents');
-evt1.initEvent('input', true, true);
-input1.value='[哈哈]';
-input1.dispatchEvent(evt1)
 
-// 点击评论按钮 提交评论
-button.click()    
+const reply = () => {
+     // 随机内容
+     const sendTextArr = ['[嘻嘻]', '[哈哈]', '5299', '冲啊！', '互动', '[舔屏]', '[doge]', '[二哈]', '周周yyds', '[心]']
+     const n = Math.floor(Math.random() * sendTextArr.length + 1) - 1;
+     const text = sendTextArr[n]
+ 
+     // 模拟输入事件 像文本框中输入内容
+     let evt1 = document.createEvent('HTMLEvents');
+     evt1.initEvent('input', true, true);
+     input1.value = text;
+     input1.dispatchEvent(evt1)
+ 
+     // 点击评论按钮 提交评论
+     button.click()
+}
+
+reply()
+
+// 十分钟评论一次
+setInterval(() => {
+    reply()
+}, 60000 * 10)
+
 
 
 
